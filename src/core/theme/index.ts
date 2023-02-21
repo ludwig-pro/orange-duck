552064657233bca4188ef26896b813dce046d895
ludwig <hello@ludwigvantours.dev>
2023-02-21 16:31:03 +0100

    theme

---
+++ src/core/theme/index.ts
@@ -0,0 +1,1 @@
+export { radii, spacing } from './theme';
---
+++ src/core/theme/theme.ts
@@ -0,0 +1,17 @@
+export const spacing = {
+  ['5']: 5,
+  ['8']: 8,
+  ['10']: 10,
+  ['15']: 15,
+  ['20']: 20,
+  ['30']: 30,
+  ['40']: 40,
+  ['80']: 80
+};
+
+export const radii = {
+  regular: 10,
+  medium: 14,
+  large: 16,
+  zero: 0
+};
